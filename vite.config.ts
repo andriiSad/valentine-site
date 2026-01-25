fecad5395c27c887a6d866de6f11e64e4a8c2244
andriiSad <andrii.sadovyi.dev2000@gmail.com>
2026-01-25 17:02:29 +0100

    Added hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Set base to './' for relative paths, or change to '/REPO_NAME/' for GitHub Pages
-  // base: '/your-repo-name/',
-  base: './',
+  base: "./",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
   },
-})
+  server: {
+    host: true,
+    allowedHosts: true,
+  },
+});
